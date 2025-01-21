125a3e7a4065227dd37a1e30e68622123c5fbd87
DEVASRI S <devasri472@gmail.com>
2025-01-21 12:14:57 +0530

    Create studentlogin.js

---
+++ loginpage/studentloginpage/studentlogin.js
@@ -0,0 +1,9 @@
+function login() {
+    // Navigate to the student dashboard
+    window.location.href = '../Studentpage/student-dashboard.html';
+}
+
+function goBack() {
+    // Navigate to the main.html page
+    window.location.href = 'main.html';
+}
