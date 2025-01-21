2c94243d8ca7fa79b85941a39cd8277ea1b4391e
DEVASRI S <devasri472@gmail.com>
2025-01-21 12:12:50 +0530

    Create facultylogin.js

---
+++ loginpage/facultyloginpage/facultylogin.js
@@ -0,0 +1,9 @@
+function login() {
+    // Navigate to the faculty dashboard
+    window.location.href = '../Facultypage/faculty_dashboard.html';
+}
+
+function goBack() {
+    // Navigate to the main.html page
+    window.location.href = 'main.html';
+}
